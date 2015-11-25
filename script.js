ea7417d825444cd83ccfa207e9bf02f779a5e1e6
John Stepan <jstep84@gmail.com>
2015-11-24 22:42:08 -0800

    submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// $(document).ready(function(){
 // just in case!
 console.log("The script link works!");
 
@@ -12,28 +13,31 @@ var dy = -2;
 
 // create the paddle
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 95;
 
-// starting point on the X axis
+// starting point on the middle of X axis
 var paddleX = (canvas.width-paddleWidth) / 2;
 
 // user-response variables
 var rightPressed = false;
 var leftPressed = false;
-var upPressed = false;
-var downPressed = false;
 
 // make the brick targets
 var brickRowCount = 3;
-var brickColumnCount = 5;
-var brickWidth = 75;
+var brickColumnCount = 7;
+var brickWidth = 50;
 var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
-// variable for score
-var score1 = 0;
+// variables for players
+var player = 1;
+var player1 = 0;
+var player2 = 0;
+
+// variables for score
+var score = 0;
 var score2 = 0;
 
 // 2D array for our bricks
@@ -57,12 +61,6 @@ function keyDownHandler(e) {
     else if (e.keyCode == 37) {
         leftPressed = true;
     }
-    else if (e.keyCode == 38) {
-        upPressed = true;
-    }
-    else if (e.keyCode == 40) {
-        downPressed = true;
-    }
 }
 
 function keyUpHandler(e) {
@@ -72,48 +70,48 @@ function keyUpHandler(e) {
     else if (e.keyCode == 37) {
         leftPressed = false;
     }
-    else if (e.keyCode == 38) {
-        upPressed = false;
-    }
-    else if (e.keyCode == 40) {
-        downPressed = false;
-    } 
 }   
 
+// detect brick collision, reverse trajectory, remove brick status, check for all bricks gone and alert winner
+function collisionDetection() {
+    for (i = 0; i < brickColumnCount; i++) {
+        for (j = 0; j < brickRowCount; j++) {
+            var b = bricks[i][j];
+            if (b.status === 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                        score++;
+                    }
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("You win the round with " + score + " points! \n Go again.");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+
 // create the ball
-function drawBall(clr) {
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = clr;
+    ctx.fillStyle = "#28546C";
     ctx.fill();
     ctx.closePath();
 }
-
-/*function colorChange() {
-    var randNum = Math.random();
-    console.log(randNum)
-    if (randNum >= .66) {
-         drawBall("red");
-    }
-    else if (randNum <= .33) {
-        drawBall("green");
-    }
-    else {
-        drawBall("#blue");
-    }
-} */
 
 // draw paddle on gameboard
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "#AA5239";
     ctx.fill();
     ctx.closePath();
 }
 
 // draw the bricks in the arena
-function drawBricks() {
+function drawBricks(player) {
     for (i = 0; i < brickColumnCount; i++) {
         for (j = 0; j < brickRowCount; j++) {
             if (bricks[i][j].status == 1) {
@@ -123,7 +121,7 @@ function drawBricks() {
                 bricks[i][j].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "blue";
+                ctx.fillStyle = "#AA5239";
                 ctx.fill();
                 ctx.closePath();
             }
@@ -131,32 +129,33 @@ function drawBricks() {
     }
 }
 
+function sBoard() {
+    document.getElementById("score").innerHTML = "Bricks smashed " +score;
+}
+
 // frame animation for the game
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawScore();
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    collisionDetection();
-    x += dx;
-    y += dy;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBricks();
+        drawBall();
+        drawPaddle();
+        sBoard();
+        collisionDetection();
+        x += dx;
+        y += dy;
 
 // wall-ball collison detection
     if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
         dx = -dx;
-        // colorChange();
     }
     if (y + dy < ballRadius) {
         dy = -dy;
-        // colorChange();
     }
     else if (y + dy > canvas.height-ballRadius) {
             if (x > paddleX && x < paddleX + paddleWidth) {
                 dy = -dy;
             }
             else {
-            alert ("You Lose!");
             document.location.reload();
         }
     }
@@ -170,38 +169,21 @@ function draw() {
     }
 }
 
-function collisionDetection() {
-    for (i = 0; i < brickColumnCount; i++) {
-        for (j = 0; j < brickRowCount; j++) {
-            var b = bricks[i][j];
-            if (b.status == 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                    dy = -dy;
-                    // colorChange();
-                    b.status = 0;
-                    score1++;
-                    if (score1 == brickRowCount * brickColumnCount) {
-                        alert("You win the round with " + score1 + " points!")
-                    }
-                }
-            }
-        }
+    function ePlay(){
+        setInterval(draw, 8);
+        menuScreen();
     }
-}
+    
+    function mPlay(){
+        setInterval(draw, 6);
+    }
+    
+    function hPlay(){
+        setInterval(draw, 4);
+    }
 
-function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "blue";
-    ctx.fillText("Player 1: " + score1, 8, 20);
-    ctx.fillText("Player 2: " + score2, 400, 20);
-}
 
-var player = 1;
-var player1 = 0;
-var player2 = 0;
-
-setInterval(draw, 15);
-
+    
 
 
 
